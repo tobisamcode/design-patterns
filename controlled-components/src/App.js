22e4379546907dc467a84c6bd2751986cc535715
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-03-18 19:55:03 +0100

    controlled components 💥

---
+++ controlled-components/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ControlledForm from "./components/controlled-form";
+
+function App() {
+  return (
+    <>
+      <ControlledForm />
+    </>
+  );
+}
+
+export default App;

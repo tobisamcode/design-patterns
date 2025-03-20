c2699729aead7453ad5f3dcd90a6a47fbd4f5c90
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-03-20 06:48:02 +0100

    Uncontrolled flows design pattern

--- uncontrolled-flows/src/App.js
+++ uncontrolled-flows/src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 
-import ControlledForm from "./components/controlled-form";
+import { UncontrolledFlow } from "./components/uncontrolled-flow";
+
+const Step1 = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1: Enter your name</h1>
+      <button onClick={() => goNext({ name: "John Doe" })}>Next</button>
+    </>
+  );
+};
+const Step2 = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #2: Enter your age</h1>
+      <button onClick={() => goNext({ age: 30 })}>Next</button>
+    </>
+  );
+};
+const Step3 = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #3: Enter your country</h1>
+      <button onClick={() => goNext({ country: "USA" })}>Next</button>
+    </>
+  );
+};
 
 function App() {
   return (
     <>
-      <ControlledForm />
+      <UncontrolledFlow
+        onDone={(data) => {
+          alert(`🎉 Yehhhh ${data.name}, you made to the final step!`);
+        }}
+      >
+        <Step1 />
+        <Step2 />
+        <Step3 />
+      </UncontrolledFlow>
     </>
   );
 }

daeba307971e955c0362a2baf2175686aacbf3ff
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-03-24 07:24:21 +0100

    # HOC

--- HOC/src/App.js
+++ HOC/src/App.js
@@ -1,5 +1,6 @@
 import { checkProps } from "./components/check-props";
 import { includeUser } from "./components/include-user";
+import { UserInfoForm } from "./components/user-form";
 import { UserInfo } from "./components/user-info";
 
 const UserInfoWrapper = checkProps(UserInfo);
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
       {/* <UserInfoWrapper propA="test1" blabla={{ a: 1, age: 23 }} />*/}
-      <UserInfoWrapperWithUser />
+      {/* <UserInfoWrapperWithUser /> */}
+      <UserInfoForm />
     </>
   );
 }

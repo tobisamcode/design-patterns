cd9b7aeb35424f1eecdd6bbc8f3db4b9dcafa4d6
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-03-27 01:41:04 +0100

    # custom hooks

---
+++ custom-hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { UserInfo } from "./components/user-info";
+
+function App() {
+  return (
+    <>
+      <UserInfo userId="1" />
+      <UserInfo userId="2" />
+      <UserInfo userId="3" />
+    </>
+  );
+}
+
+export default App;
